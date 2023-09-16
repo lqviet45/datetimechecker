3d8504e8bb968242616846aa8b1d92c35ec3d8e6
lqviet45 <lqviet455@gmail.com>
2023-09-16 08:48:45 +0700

    Add project files.

---
+++ DateTimeChecker/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using DateTimeChecker.Model;
+using DateTimeChecker.ServiceContract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DateTimeChecker.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IDateTimeService _dateTimeService;
+
+        public HomeController(IDateTimeService dateTimeService)
+        {
+            _dateTimeService = dateTimeService;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/checkday")]
+        public IActionResult CheckDay(Date date)
+        {
+            var totalDayInMonth = _dateTimeService.CheckDayInMonth(date.Month, date.Year);
+            if (totalDayInMonth == 0 || date.Day > totalDayInMonth)
+            {
+                ViewBag.Result = $"{date} is Not correct date time!";
+                return View();
+            }
+            ViewBag.Result = $"{date} is correct day time!";
+            return View(date);
+        }
+    }
+}
---
+++ DateTimeChecker/Model/Date.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DateTimeChecker.Model
+{
+    public class Date
+    {
+        [Required(ErrorMessage = "Input data for day is inccorect format")]
+        [Range(maximum: 31, minimum: 1)]
+        public int Day { get; set; }
+
+        [Required(ErrorMessage = "Input month for month is inccorect format")]
+        [Range(maximum: 12, minimum: 1)]
+        public int Month { get; set; }
+
+        [Required(ErrorMessage = "Input year for year is inccorect format")]
+        [Range(maximum: 3000, minimum: 1000)]
+        public int Year { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Day}/{Month}/{Year}";
+        }
+    }
+}
